0cf844c311b689afe44cc07a29d8cbb189bdcbe5
jaxonhill <jaxoncharleshill@gmail.com>
2023-05-12 11:44:10 -0700

    Move dark mode state to document for body color

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,19 +8,14 @@ import Navbar from "@/components/Navbar";
 const poppins = Poppins({ subsets: ["latin"], weight: ["400"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-	// Hold state of if we are using dark mode or not, false by default
-	const [isDarkMode, setIsDarkMode] = useState(true);
-
 	return (
-		<div className={`${poppins.className}`}>
+		<div className="font-poppins">
 			<Navbar />
 			{/* Set the whole website to dark mode if the dark mode state is true
       by applying "dark" class name on the main DOM element wrapping everything */}
 			{/* <main className={`${isDarkMode && "dark"}`}> */}
 			<main className="px-4 mb-8">
-				<section className={`dark:bg-slate-900`}>
-					<Component {...pageProps} />
-				</section>
+				<Component {...pageProps} />
 			</main>
 		</div>
 	);
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,18 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { useState } from "react";
+import { Poppins } from "next/font/google";
 
 export default function Document() {
+	// Hold state of if we are using dark mode or not, false by default
+	const [isDarkMode, setIsDarkMode] = useState(false);
 	return (
 		<Html lang="en">
 			<Head />
-			<body className="bg-gray-200">
-				<Main />
-				<NextScript />
+			<body className={`${isDarkMode && "dark"}`}>
+				<div className={isDarkMode ? "bg-slate-900" : "bg-gray-200"}>
+					<Main />
+					<NextScript />
+				</div>
 			</body>
 		</Html>
 	);
